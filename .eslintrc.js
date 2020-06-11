c5e7f13a31ff5833996eea4ac8618d65e80f5de5
Зубков Валерий Павлович <v.zubkov@bssys.com>
2020-06-11 22:12:26 +0400

    Добавил eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const path = require('path');
+
+const webpackConfigPath = './webpack.config.js';
+
+module.exports = {
+  extends: ['airbnb'],
+  parser: 'babel-eslint',
+  globals: {
+    __DEV__: true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: path.resolve(__dirname, webpackConfigPath),
+      },
+    },
+  },
+  plugins: ['react', 'react-hooks'],
+};
