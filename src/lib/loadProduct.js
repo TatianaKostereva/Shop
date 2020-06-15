de20dbab63991d314d200dbee44292614d27c842
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-06-15 15:19:32 +0300

    loadProduct

---
+++ src/lib/loadProduct.js
@@ -0,0 +1,16 @@
+const loadProduct = (productsUrl) => {
+  fetch(productsUrl)
+    .then((res) => res.json())
+    .then((data) => data.map((product) => {
+      const temp = product.price.split(' ');
+      product.price = temp[1];
+      product.currency = temp[0];
+      return {
+        ...product,
+        price: temp[1],
+        currency: temp[0],
+      };
+    }));
+};
+
+export default loadProduct;
