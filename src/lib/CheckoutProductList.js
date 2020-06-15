36e7d2b34df8b4f21963053b727c242c69b76ac0
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-06-15 19:44:37 +0300

    исправила передачу данных, в localStorage передаю id продуктов

--- src/lib/CheckoutProductList.js
+++ src/lib/CheckoutProductList.js
@@ -1,91 +1,43 @@
-'use strict';
-
 class CheckoutProductList {
-  productsStoreKey = 'cart-products';
-
-  constructor(parentElement) {
-    this.productsUrl = '/assets/data/products.json';
+  constructor(parentElement, products) {
     this.el = parentElement;
-    this.load().then((products) => {
-      console.log(products)
-      this.getProducts(products);
-      this.getSumPrice();
-      this.getArray();
-      this.show();
+    this.products = products;
+    this.getProducts();
+    this.show().then(() => {
       this.initListeners();
     });
   }
 
-  load() {
-    return fetch(this.productsUrl)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        this.data = data;
-        
-        return this.data;
-      })
-      .then((products) => {
-        
-        products.map(product => {
-        const temp = product.price.split(/\B\s/);
-        product.price = temp[1];
-        product.currency = temp[0];
-        console.log(product)
-        return { 
-          ...product,
-          price: temp[1],
-          currency: temp[0]
-        }
-        });
-        console.log(products)
-        return products;
-      })
-  }
-
-  getProducts(products) {
-    
+  getProducts = () => {
     const productsLocalStorage = localStorage.getItem('cart-products');
     const idProductsInCart = JSON.parse(productsLocalStorage) || [];
-    
-    console.log(idProductsInCart)
-      let productsInCart = [];
-      for (let id of idProductsInCart) {
-        productsInCart = products.filter(el => el.id == +id);
-      }
-      console.log(productsInCart);
-  
+
+    const productsInCart = idProductsInCart.map((id) => {
+      const result = this.products.find((product) => product.id == id);
+      return result;
+    });
     return productsInCart;
-    
-  }
+  };
 
-  getSumPrice() {
-    
-    const products = this.getProducts();
-    const arr = products.map(item => item.price);
-
-    console.log(arr);
+  getSumPrice = async () => {
+    const products = await this.getProducts();
+    const arr = products.map((item) => item.price);
 
     let sum = 0;
-    for (let item of arr) {
-      sum+= +item;
+    for (const item of arr) {
+      sum += +item;
     }
-      
-    console.log(sum);
+
     return sum.toFixed(2);
-    
   }
 
-  getRate(item) {
-    
+  getRate = (item) => {
     const stars = new Array(5).fill('').map((value, index) => {
       if (item.rating != null) {
-        let modificator = index < item.rating.stars ? 'checked' : 'active';
-        return `<i class="icon-star ${modificator}"></i>`
-      } else {
-        return `<i class="icon-star"></i>`
+        const modificator = index < item.rating.stars ? 'checked' : 'active';
+        return `<i class="icon-star ${modificator}"></i>`;
       }
+      return '<i class="icon-star"></i>';
     }).join('');
     const reviews = item.rating === null ? 0 : item.rating.reviewsAmount;
 
@@ -94,48 +46,39 @@ class CheckoutProductList {
             ${stars}
         </div>
         <p class="rate-amount d-none d-md-block mt-1">${reviews} reviews <p>
-    `
-    
+    `;
   }
 
-  getPrice(item) {
-    return `
+  getPrice = (item) => `
     <div class="product-price">
       <p class="mb-0 font-weight-light">Price:</p>
       <h4 class="col-title price-text mb-2">${item.currency} ${item.price}</h4>
     </div>
-    `;
-  }
+    `
 
-  getArray() {
-   
-    const arrayOfProductsInCart = this.getProducts();
+  getArray = async () => {
+    const arrayOfProductsInCart = await this.getProducts();
 
-    const map = arrayOfProductsInCart.reduce(function (obj, item) {
+    const map = arrayOfProductsInCart.reduce((obj, item) => {
       if (!obj.hasOwnProperty(item.id)) {
         obj[item.id] = item;
-        obj[item.id]['sum'] = 0;
+        obj[item.id].sum = 0;
       }
-      obj[item.id]['sum']++;
+      obj[item.id].sum++;
       return obj;
     }, {});
 
-    console.log(map);
-
     const resultArray = [];
-    for(let obj in map) { 
-      resultArray.push(map[obj]); 
+    for (const obj in map) {
+      resultArray.push(map[obj]);
     }
 
-    console.log(resultArray)
     return resultArray;
-    
   }
 
-  render() {
+  render = async () => {
+    const array = await this.getArray();
 
-    const array = this.getArray();
-    console.log(array)
     const products = array.map((item) => {
       const rate = this.getRate(item);
       const price = this.getPrice(item);
@@ -161,16 +104,16 @@ class CheckoutProductList {
           </div>
         </div>
       `;
-        
+
       return product;
     }).join('');
     return products;
   }
 
-  show() {
-    const render = this.render();
-    const sumPrice = this.getSumPrice();
-    console.log(sumPrice)
+  show = async () => {
+    const render = await this.render();
+    const sumPrice = await this.getSumPrice();
+
     const productsInCart = `
     <div class="product-list-box">
       ${render}
@@ -178,48 +121,47 @@ class CheckoutProductList {
     <div class="order-value"> Order value: € ${sumPrice}</div>
     `;
 
-    return this.el.innerHTML = productsInCart;
+    this.el.innerHTML = productsInCart;
+
+    return productsInCart;
   }
 
-  initListeners() {
-    this.el.querySelector('.product-list-box').addEventListener('click', event => {
+  initListeners = () => {
+    this.el.querySelector('.product-list-box').addEventListener('click', (event) => {
       const { target } = event;
-     
+
       if (target.getAttribute('data-button-role') !== 'checkout-remove-product') {
         return false;
       }
       if (confirm('Вы уверены, что хотите удалить этот товар из корзины?') !== true) {
         return false;
-      } 
+      }
       const elem = target.closest('.product-wrapper');
       const id = elem.getAttribute('data-product-id');
 
       this.deleteProduct(id);
-        
     });
   }
 
-  deleteProduct(id) {
-    const products = this.getProducts();
-
+  deleteProduct = async (id) => {
+    const products = await this.getProducts();
     const deletedElement = this.el.querySelector(`div[data-product-id='${id}']`);
-  
     const quantityEl = this.el.querySelector(`div[data-product-id='${id}'] > div.quantity > h1`);
 
     let quantity = quantityEl.innerText;
-    if (quantity  > 1 ) {
+    if (quantity > 1) {
       quantity -= 1;
-      quantityEl.innerText = quantity ;
+      quantityEl.innerText = quantity;
     } else {
       deletedElement.remove();
     }
-    
-    const indexOfProductToDelete = products.findIndex(product => product.id == id);
-    products.splice(indexOfProductToDelete,1);
-  
-    localStorage.setItem('cart-products', JSON.stringify(products));
-  }
 
+    const indexOfProductToDelete = products.findIndex((product) => product.id == id);
+    products.splice(indexOfProductToDelete, 1);
+    const newIndexesOfProducts = products.map((product) => product.id);
+
+    localStorage.setItem('cart-products', JSON.stringify(newIndexesOfProducts));
+  }
 }
 
 export default CheckoutProductList;
