701c3734d604ba2b50d29da0063904991944cbc1
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-07-03 16:26:14 +0300

    переписала код

--- src/lib/Menu.js
+++ src/lib/Menu.js
@@ -5,8 +5,8 @@ class Menu extends React.Component {
     super(props);
 
     this.state = {
-      cameraPhotos: 0,
-      cinema: 0,
+      cameraPhotos: false,
+      cinema: false,
     }
   }
 
@@ -16,14 +16,14 @@ class Menu extends React.Component {
 
     return (
       <ul className ="list-group sidebar">
-        <li className="list-group-item dropdown" data-menu-to="1" onPointerEnter = {this.showDropdownMenu} onPointerLeave = {this.hideDropdownMenu}>
+        <li className="list-group-item dropdown" data-menu-to="cameraPhotos" onPointerEnter = {this.showDropdownMenu} onPointerLeave = {this.hideDropdownMenu}>
           <a className="nav-link dropdown-toggle" id="cameraPhotos">Camera &amp; Photo</a>
           <ul className={classNameCameraPhotos}>
            <li data-id="cameraPhotos_accessories" className="dropdown-item"><a>Accessories</a></li>
           </ul>
         </li>
 
-        <li className="list-group-item dropdown"  data-menu-to="2" onPointerEnter = {this.showDropdownMenu} onPointerLeave = {this.hideDropdownMenu}>
+        <li className="list-group-item dropdown"  data-menu-to="cinema" onPointerEnter = {this.showDropdownMenu} onPointerLeave = {this.hideDropdownMenu}>
           <a className="nav-link dropdown-toggle" id="cinema">Home Cinema, TV &amp; Video</a>
           <ul className={classNameCinema}>
            <li data-id="cinema_audio" className="dropdown-item"><a>Audio</a></li>
@@ -35,36 +35,22 @@ class Menu extends React.Component {
   }
 
   showDropdownMenu = (event) => {
-    const { target } = event;
-    const currentMenuIndex = +event.currentTarget.dataset.menuTo;
+    const currentMenuIndex = event.currentTarget.dataset.menuTo;
 
-    if (currentMenuIndex === 1) {
-      this.setState({
-        cameraPhotos: this.state.cameraPhotos + 1,
-      });
-    } else {
-      this.setState({
-        cinema: this.state.cinema + 1,
-      });
-    }
+    this.setState({
+      [currentMenuIndex]: true,
+    });
 
     const backdrop = document.querySelector('.backdrop');
     backdrop.classList.add('show');
   };
 
   hideDropdownMenu = (event) => {
-    const { target } = event;
-    const currentMenuIndex = +event.currentTarget.dataset.menuTo;
+    const currentMenuIndex = event.currentTarget.dataset.menuTo;
 
-    if (currentMenuIndex === 1) {
-      this.setState({
-        cameraPhotos: this.state.cameraPhotos - 1,
-      });
-    } else {
-      this.setState({
-        cinema: this.state.cinema - 1,
-      });
-    }
+    this.setState({
+      [currentMenuIndex]: false,
+    });
 
     const backdrop = document.querySelector('.backdrop');
     backdrop.classList.remove('show');
