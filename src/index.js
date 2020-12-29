4c4c7b3db9f7cb13dc9e28fb170861b4a6899482
TatianaKostereva <tatjana.kostereva@yandex.ru>
2020-12-29 21:38:05 +0300

    fix DBComponent

--- src/index.js
+++ src/index.js
@@ -9,23 +9,20 @@ import {
   Route,
 } from 'react-router-dom';
 import DBCurrency from '@/db/DBCurrency';
-import DBReviews from '@/db/DBReviews';
 import DBComponent from '@/db/DBComponent';
 
 ReactDOM.render(
   (
     <Router>
       <DBComponent>
-        <DBReviews>
-          <DBCurrency>
-            <DBCart>
-              <Switch>
-                <Route exact path="/" component={MainPage} />
-                <Route exact path="/cart" component={CartPage} />
-              </Switch>
-            </DBCart>
-          </DBCurrency>
-        </DBReviews>
+        <DBCurrency>
+          <DBCart>
+            <Switch>
+              <Route exact path="/" component={MainPage} />
+              <Route exact path="/cart" component={CartPage} />
+            </Switch>
+          </DBCart>
+        </DBCurrency>
       </DBComponent>
     </Router>
   ),
